8bda309cbfa0d06898c273de02569e9db855675f
Man-Chun Hui <manchhui@gmail.com>
2020-09-14 16:44:36 +0100

    Create __init__.py
    type: feat
    
    Setup plugins init.py file and input custom operator names for this project.

---
+++ plugins/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import division, absolute_import, print_function
+
+from airflow.plugins_manager import AirflowPlugin
+
+import operators
+import helpers
+
+# Defining the plugin class
+class UdacityPlugin(AirflowPlugin):
+    name = "udacity_plugin"
+    operators = [
+        operators.StageToRedshiftOperatorCAP,
+        operators.LoadFactOperatorCAP,
+        operators.LoadDimensionOperatorCAP,
+        operators.DataQualityOperatorCAP
+    ]
+    helpers = [
+        helpers.SqlQueries
+    ]
